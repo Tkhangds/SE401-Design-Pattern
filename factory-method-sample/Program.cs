12b08366af4a3c75aefbb1949cbf9bdb4cfe93aa
Tkhangds <gunnyvippro111@gmail.com>
2025-03-03 23:25:15 +0700

    Initial commit

---
+++ factory-method-sample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+abstract class Logistics
+{
+    public abstract Transport CreateTransport();
+    
+    public void SomeMethod()
+    {
+        Transport transport = CreateTransport();
+        Console.WriteLine($"Using transport: {transport.Deliver()}");
+    }
+}
+
+class RoadLogistics : Logistics
+{
+    public override Transport CreateTransport()
+    {
+        return new Truck();
+    }
+}
+
+class SeaLogistics : Logistics
+{
+    public override Transport CreateTransport()
+    {
+        return new Ship();
+    }
+}
+interface Transport
+{
+    string Deliver();
+}
+
+class Truck : Transport
+{
+    public string Deliver()
+    {
+        return "Deliver On Road!";
+    }
+}
+class Ship : Transport
+{
+    public string Deliver()
+    {
+        return "Deliver On Ship!";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Road Logistics:");
+        Logistics roadLogistics = new RoadLogistics();
+        roadLogistics.SomeMethod();
+        
+        Console.WriteLine("\nSea Logistics:");
+        Logistics seaLogistics = new SeaLogistics();
+        seaLogistics.SomeMethod();
+    }
+}
